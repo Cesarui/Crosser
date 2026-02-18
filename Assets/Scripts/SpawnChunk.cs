dcd06f27a881042d663e3b79bca403786a2775a1
Cesar Pimentel <cpimentelortiz23@gmail.com>
2026-02-18 02:34:55 -0500

    feat: add max chunk limit to chunk spawner

--- Assets/Scripts/SpawnChunk.cs
+++ Assets/Scripts/SpawnChunk.cs
@@ -5,13 +5,18 @@ public class SpawnChunk : MonoBehaviour
     public GameObject chunk;
 
     public GameObject lastChunk;
+
+    public int chunkCounter = 0;
+    public int maxChunkAmount = 12;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player" && chunkCounter < maxChunkAmount)
         {
+            chunkCounter++;
             Instantiate(chunk, new Vector3(chunk.transform.position.x - 45, 0, 0), chunk.transform.rotation);
 
             lastChunk = chunk;
+            Debug.Log(chunkCounter.ToString());
         }
     }
 
