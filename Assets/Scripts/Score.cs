0f78376b64f2b3940fe819164d31bfd8254b092a
Cesar Pimentel <cpimentelortiz23@gmail.com>
2026-02-18 02:31:54 -0500

    feat: add score multiplier and return absolute value to avoid negative distance

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -4,12 +4,15 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    public float multiplier = 1.5f;
     public Transform player;
     public TextMeshProUGUI scoreText;
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = player.position.x.ToString();
+        float xPos = player.position.x;
+        float positiveX = Mathf.Abs(xPos * multiplier);
+        scoreText.text = positiveX.ToString("0");
     }
 }
