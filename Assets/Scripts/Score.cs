0223efc2b669bc5da22393daca01dfab2ae17200
Cesar Pimentel <cpimentelortiz23@gmail.com>
2026-02-21 00:09:53 -0500

    style: show meters in scoreText UI, e.g., 500m

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -8,12 +8,15 @@ public class Score : MonoBehaviour
     public float multiplier = 1.5f;
     public Transform player;
     public TextMeshProUGUI scoreText;
-
+    float xPos;
+    private void Start()
+    {
+    }
     // Update is called once per frame
     void Update()
     {
-        float xPos = player.position.x;
+        xPos = player.position.x;
         float positiveX = Mathf.Abs(xPos * multiplier);
-        scoreText.text = positiveX.ToString("0");
+        scoreText.text = positiveX.ToString("0m");
     }
 }
