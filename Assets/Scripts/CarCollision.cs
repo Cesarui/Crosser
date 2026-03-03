cbf04d61312ea53bef6a01cc5ae506e6cb7c5632
Cesar Pimentel <cpimentelortiz23@gmail.com>
2026-03-03 18:37:53 -0500

    feat(audio): add crash sound on front collision

--- Assets/Scripts/CarCollision.cs
+++ Assets/Scripts/CarCollision.cs
@@ -3,12 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class CarCollision : MonoBehaviour
 {
+    public GameObject crashAudioPrefab;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            Debug.Log("You Died");
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            GameObject audioObj = Instantiate(crashAudioPrefab, transform.position, Quaternion.identity);
+            AudioSource audio = audioObj.GetComponent<AudioSource>();
+            audio.Play();
+            
+            Destroy(audioObj, audio.clip.length);
         }
     }
 }
